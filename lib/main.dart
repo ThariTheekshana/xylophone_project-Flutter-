4ad98c33308ee4d31010f826cae6eb084148a4b2
Tharindu Theekshana <138811687+ThariTheekshana@users.noreply.github.com>
2023-12-17 14:39:30 +0530

    xylophone_project in flutter

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(const XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  void playSound(soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          child: const Text('Click Me 1'),
+          onPressed: () {
+            playSound(soundNumber);
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildKey(color: Colors.red, soundNumber: 1),
+                buildKey(color: Colors.orange, soundNumber: 2),
+                buildKey(color: Colors.yellow, soundNumber: 3),
+                buildKey(color: Colors.green, soundNumber: 4),
+                buildKey(color: Colors.blue, soundNumber: 5),
+                buildKey(color: Colors.indigo, soundNumber: 6),
+                buildKey(color: Colors.purple, soundNumber: 7),
+              ],
+            ),
+          )),
+    );
+  }
+}_
